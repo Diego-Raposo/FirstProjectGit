ba0fdf70014ba037b094ebe9e465d18836612fc6
SarahSilvaSales <sarasilvasales@gmail.com>
2023-05-29 11:12:17 -0400

    Initial Commit

---
+++ FirstProjectGit/FirstProjectGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectGitApp.swift
+//  FirstProjectGit
+//
+//  Created by projeto smart on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
